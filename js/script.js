10465035002e0e41bbb68c24b8e89a99e4de812d
MaciejKut <maciej.kutzmann@gmail.com>
2019-02-17 18:28:53 +0100

    First exercise XMLHttp

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var url = "http://api.icndb.com/jokes/random";
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function () {
+    getJoke();
+});
+
+var container = document.getElementById('joke');
+
+function getJoke() {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', function () {
+        var response = JSON.parse(request.response);
+        if (request.status == 200) {
+            container.classList.add('bgwhite');
+            container.innerHTML = response.value.joke;
+        }
+    });
+
+    request.send();
+};
